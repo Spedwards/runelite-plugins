e2515649c86f641448f7c826ea0be2a4af7afcb6
mlgudi <seriousgudi@gmail.com>
2020-01-07 10:16:20 +0000

    Add copyright headers (sry) Add repaint to catch deletion of final account from config

--- src/main/java/com/profiles/Profiles/ProfilesPanel.java
+++ src/main/java/com/profiles/Profiles/ProfilesPanel.java
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2019, Spedwards <https://github.com/Spedwards>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.profiles.Profiles;
 
 import java.awt.Dimension;
@@ -137,7 +161,6 @@ class ProfilesPanel extends PluginPanel
                 return;
             }
             String data = labelText + ":" + loginText;
-            System.out.println(data);
             this.addAccount(data);
 
             addProfile(data);
@@ -193,10 +216,11 @@ class ProfilesPanel extends PluginPanel
 
     void addAccounts(String data)
     {
-        System.out.println("Data: " + data);
         data = data.trim();
         if (!data.contains(":"))
         {
+            revalidate();
+            repaint();
             return;
         }
         Arrays.stream(data.split("\\n")).forEach(this::addAccount);
